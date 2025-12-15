cc693b1a5a70a9951eced88d6942a3b4ae84fd9f
jonatas06ap <jonatasdepaula06@gmail.com>
2025-12-15 19:16:36 -0300

    Implementação multiplação e divisão

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+# CalcularITA
+
+def multiplicacao(a, b):
+    return a * b
+def divisao(a, b):
+    return a / b
+
+#Teste das funções
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 5
+    print("Multiplicação:", multiplicacao(num1, num2))
+    print("Divisão:", divisao(num1, num2))
