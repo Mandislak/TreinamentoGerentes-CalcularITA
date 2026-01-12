743b44acec0c100cbab39081953ee7f0d21cbaf8
Leonardo Cordeiro Silva <leonardo.cordeiro@itajunior.com.br>
2026-01-12 15:43:20 -0300

    Adicionado as funções de adição e subtração

--- calculadora.py
+++ calculadora.py
@@ -4,6 +4,10 @@ def multiplicacao(a, b):
     return a * b
 def divisao(a, b):
     return a / b
+def adicao(a, b):
+    return a + b
+def subtracao(a, b):
+    return a - b
 
 #Teste das funções
 if __name__ == "__main__":
@@ -11,3 +15,5 @@ if __name__ == "__main__":
     num2 = 5
     print("Multiplicação:", multiplicacao(num1, num2))
     print("Divisão:", divisao(num1, num2))
+    print("Adição:", adicao(num1, num2))
+    print("Subtração:", subtracao(num1, num2))
